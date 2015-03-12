24f97d60c942d1b9ffcfc4e097c19a47a8b216e0
Iván Corrales Solera <developer@wesovi.com>
2015-03-12 11:45:04 +0100

    feat: adding kibana configuration

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM nginx:1.7
+MAINTAINER Innersource
+
+ENV KIBANA_PKG  kibana-4.0.0-linux-x64
+ENV KIBANA_HOME /opt/kibana
+
+ADD https://download.elasticsearch.org/kibana/kibana/$KIBANA_PKG.tar.gz /tmp/kibana.tar.gz
+ADD bin/run.sh /usr/local/bin/run
+
+
+RUN mkdir -p $KIBANA_HOME
+RUN tar -xvzf /tmp/kibana.tar.gz && mv $KIBANA_PKG/* /opt/kibana 
+
+COPY config/kibana.yml /opt/kibana/config/kibana.yml
+COPY bin/run.sh /usr/local/bin/run
+RUN chmod +x /usr/local/bin/run
+COPY bin/kibana /opt/kibana/bin/kibana
+RUN chmod +x /opt/kibana/bin/kibana
+EXPOSE 5601
+
+WORKDIR $KIBANA_HOME
+
+CMD /opt/kibana/bin/kibana
---
+++ bin/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+ES_HOST=${ES_HOST:-\"+window.location.hostname+\"}
+ES_PORT=${ES_PORT:-9200}
+ES_SCHEME=${ES_SCHEME:-http}
+
+# disable ipv6 support
+if [ ! -f /proc/net/if_inet6 ]; then
+  sed -e '/listen \[::\]:80/ s/^#*/#/' -i /etc/nginx/sites-enabled/*
+fi
+
+cat << EOF > /usr/share/nginx/html/config.js
+define(['settings'],
+function (Settings) {
+  return new Settings({
+    elasticsearch: "$ES_SCHEME://$ES_HOST:$ES_PORT",
+    default_route     : '/dashboard/file/logstash.json',
+    kibana_index: "kibana-int",
+    panel_names: [
+      'histogram',
+      'map',
+      'goal',
+      'table',
+      'filtering',
+      'timepicker',
+      'text',
+      'hits',
+      'column',
+      'trends',
+      'bettermap',
+      'query',
+      'terms',
+      'stats',
+      'sparklines'
+    ]
+  });
+});
+EOF
+
+nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
